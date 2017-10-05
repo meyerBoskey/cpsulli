8c512508e932ee6beb8f851094d3f5cf6b516250
Cullen <cpsulli1991@gmail.com>
2017-10-05 12:40:53 -0500

    please work

--- public/js/app/messages/message.service.js
+++ public/js/app/messages/message.service.js
@@ -27,7 +27,7 @@ var MessageService = /** @class */ (function () {
         var token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.post('http://cpsulli.herokuapp.com/message' + token, body, { headers: headers })
+        return this.http.post('https://cpsulli.herokuapp.com/message' + token, body, { headers: headers })
             .map(function (response) {
             var result = response.json();
             var message = new Message(result.obj.content, result.user.firstName, result.obj._id, result.user._id);
@@ -41,7 +41,7 @@ var MessageService = /** @class */ (function () {
     };
     MessageService.prototype.getMessages = function () {
         var _this = this;
-        return this.http.get('http://cpsulli.herokuapp.com/message')
+        return this.http.get('https://cpsulli.herokuapp.com/message')
             .map(function (response) {
             var messages = response.json().obj;
             var transformedMessages = [];
@@ -68,7 +68,7 @@ var MessageService = /** @class */ (function () {
         var token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.patch('http://cpsulli.herokuapp.com/message/' + message.messageId + token, body, { headers: headers })
+        return this.http.patch('https://cpsulli.herokuapp.com/message/' + message.messageId + token, body, { headers: headers })
             .map(function (response) { return response.json(); })
             .catch(function (error) {
             _this.errorService.handelError(error.json());
@@ -81,7 +81,7 @@ var MessageService = /** @class */ (function () {
         var token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.delete('http://cpsulli.herokuapp.com/message/' + message.messageId + token)
+        return this.http.delete('https://cpsulli.herokuapp.com/message/' + message.messageId + token)
             .map(function (response) { return response.json(); })
             .catch(function (error) {
             _this.errorService.handelError(error.json());
