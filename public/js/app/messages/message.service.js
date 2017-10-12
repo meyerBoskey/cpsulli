d63aed114d825fcca889c26bc9ab044f7a82aa68
Cullen <cpsulli1991@gmail.com>
2017-10-12 11:51:59 -0500

    updated messages to only show messages belonging to the logged in user

--- public/js/app/messages/message.service.js
+++ public/js/app/messages/message.service.js
@@ -27,7 +27,7 @@ var MessageService = /** @class */ (function () {
         var token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.post('http://localhost:3000/message' + token, body, { headers: headers })
+        return this.http.post('https://cpsulli.herokuapp.com/message' + token, body, { headers: headers })
             .map(function (response) {
             var result = response.json();
             var message = new Message(result.obj.content, result.user.firstName, result.obj._id, result.user._id);
@@ -44,7 +44,7 @@ var MessageService = /** @class */ (function () {
         var token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.get('http://localhost:3000/message' + token)
+        return this.http.get('https://cpsulli.herokuapp.com/message' + token)
             .map(function (response) {
             var messages = response.json().obj;
             console.log(messages);
@@ -72,7 +72,7 @@ var MessageService = /** @class */ (function () {
         var token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.patch('http://localhost:3000/message/' + message.messageId + token, body, { headers: headers })
+        return this.http.patch('https://cpsulli.herokuapp.com/message/' + message.messageId + token, body, { headers: headers })
             .map(function (response) { return response.json(); })
             .catch(function (error) {
             _this.errorService.handleError(error.json());
@@ -85,7 +85,7 @@ var MessageService = /** @class */ (function () {
         var token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.delete('http://localhost:3000/message/' + message.messageId + token)
+        return this.http.delete('https://cpsulli.herokuapp.com/message/' + message.messageId + token)
             .map(function (response) { return response.json(); })
             .catch(function (error) {
             _this.errorService.handleError(error.json());
